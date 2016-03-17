936e9398c8c95ca1443b34449f01446ff67f9351
Kai Tallafus <ktallafus@dachcom.ch>
2016-03-17 17:46:29 +0100

    Template Pfad Lightbox Image korrigiert

--- views/scripts/toolbox/image/lightbox.php
+++ views/scripts/toolbox/image/lightbox.php
@@ -1,3 +1,3 @@
 <a href="<?= $this->image("ci", array("thumbnail" => "contentImage"))->getSrc(); ?>" class="item">
-    <?php $this->template('image/single.php', array('class' => 'img-responsive ' . $this->class)); ?>
+    <?php $this->template('toolbox/image/single.php', array('class' => 'img-responsive ' . $this->class)); ?>
 </a>
