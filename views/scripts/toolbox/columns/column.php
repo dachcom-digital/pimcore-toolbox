b59d5648b7c31a3ffa71a99c65e225bb6ce8b0b5
Kai Tallafus <ktallafus@dachcom.ch>
2016-04-12 16:58:54 +0200

    add offset syntax for column definition

--- views/scripts/toolbox/columns/column.php
+++ views/scripts/toolbox/columns/column.php
@@ -1,8 +1,17 @@
 <?php foreach ($this->columns as $i => $column) { ?>
 
-    <?php $name = 'column' . $column . '_' . $i; ?>
+    <?php
 
-    <div class="col-md-<?= $column ?> col-sm-<?= $column ?> col-xs-12">
+    if ( substr($column, 0, 1) == 'o' ) {
+        $offset = (int)substr($column, -1);
+        continue;
+    }
+
+    $name = 'column' . $column . '_' . $i;
+
+    ?>
+
+    <div class="col-md-<?= $column ?> col-sm-<?= $column ?><?php if ( $offset ) {?> col-md-offset-<?=$offset?> col-sm-offset-<?=$offset?><?php } ?> col-xs-12">
 
         <div class="toolbox-column<?php echo $this->equalHeight ? ' equal-height-item' : '' ?>">
 
@@ -11,5 +20,7 @@
         </div>
 
     </div>
+
+    <?php if ( $offset ) unset($offset); ?>
 
 <?php } ?>
