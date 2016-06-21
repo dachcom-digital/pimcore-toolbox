6619aaeac20733e907b901488d26a04971d71b5f
Kai Tallafus <ktallafus@dachcom.ch>
2016-06-21 11:11:10 +0200

    added missing equal-height class to toolbox-columns wrapper

--- views/areas/columns/view.php
+++ views/areas/columns/view.php
@@ -1,6 +1,9 @@
 <?php if( $this->editmode ) { ?>
     <?= \Toolbox\Tools\ElementBuilder::buildElementConfig('columns', $this) ?>
 <?php }?>
-<div class="toolbox-columns <?= $this->select('columnsAdditionalClasses')->getData();?>">
+<?php
+$equalHeight = $this->checkbox('equalHeight')->isChecked() && !$this->editmode;
+?>
+<div class="toolbox-columns <?= $this->select('columnsAdditionalClasses')->getData();?><?= $equalHeight ? ' equal-height' : ''?>">
     <?= $this->template('toolbox/columns.php') ?>
 </div>
