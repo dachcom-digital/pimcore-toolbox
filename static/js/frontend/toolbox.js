67c95d879cabff2554c14006be71a2380a7bdedb
Stefan Hagspiel <shagspiel@dachcom.ch>
2016-10-03 11:00:34 +0200

    code formatting

--- static/js/frontend/toolbox.js
+++ static/js/frontend/toolbox.js
@@ -1,10 +1,10 @@
 /*
- __           __                                ___       _ __        __
- ____/ /___ ______/ /_  _________  ____ ___     ____/ (_)___ _(_) /_____ _/ /
- / __  / __ `/ ___/ __ \/ ___/ __ \/ __ `__ \   / __  / / __ `/ / __/ __ `/ /
+        __           __                                ___       _ __        __
+   ____/ /___ ______/ /_  _________  ____ ___     ____/ (_)___ _(_) /_____ _/ /
+  / __  / __ `/ ___/ __ \/ ___/ __ \/ __ `__ \   / __  / / __ `/ / __/ __ `/ /
  / /_/ / /_/ / /__/ / / / /__/ /_/ / / / / / /  / /_/ / / /_/ / / /_/ /_/ / /
  \__,_/\__,_/\___/_/ /_/\___/\____/_/ /_/ /_/   \__,_/_/\__, /_/\__/\__,_/_/
- /____/
+                                                       /____/
  copyright @ 2016, dachcom digital
 
  */
@@ -50,12 +50,12 @@ var DachcomToolbox = (function () {
 
             var $maps = $('.toolbox-googlemap'),
 
-            //internal callback 1
+                //internal callback 1
                 isValidLocation = function(location) {
                     return location.hasOwnProperty('lat') && location.hasOwnProperty('lng') && !isNaN(location.lat) && !isNaN(location.lng);
                 },
 
-            //internal callback 2
+                //internal callback 2
                 addMarker = function(map, location, markerIcon) {
 
                     var marker = new google.maps.Marker({
