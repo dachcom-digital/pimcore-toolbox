9af25a75651657be3e0f17cbdfe29ec057336b93
Stefan Hagspiel <shagspiel@dachcom.ch>
2016-12-14 19:29:38 +0100

    allow custom toolbar configuration

--- views/scripts/admin/settings/ckeditor-area-style.php
+++ views/scripts/admin/settings/ckeditor-area-style.php
@@ -1,7 +1,13 @@
-CKEDITOR.editorConfig = function( config ) {
-    <?php if( is_array( $this->config ) ) { ?>
-        <?php foreach( $this->config as $configName => $configData ) { ?>
-            config.<?= $configName; ?> = <?= json_encode( $configData ); ?>;
-        <?php } ?>
+<?php if( isset( $this->globalStyleSets ) && !empty( $this->globalStyleSets ) ) { ?>
+    <?php foreach( $this->globalStyleSets as $stylesSetName => $stylesSetData ) { ?>
+        CKEDITOR.stylesSet.add('<?= $stylesSetName; ?>', <?= json_encode( $stylesSetData ); ?>);
     <?php } ?>
+<?php } ?>
+
+CKEDITOR.editorConfig = function( config ) {
+<?php if( is_array( $this->config ) ) { ?>
+    <?php foreach( $this->config as $configName => $configData ) { ?>
+        config.<?= $configName; ?> = <?= json_encode( $configData ); ?>;
+    <?php } ?>
+<?php } ?>
 };
