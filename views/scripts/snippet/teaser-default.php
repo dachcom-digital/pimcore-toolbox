ec7291b0e4442cbe7e60818193b4eda743821eae
Kai Tallafus <ktallafus@dachcom.ch>
2016-12-19 17:57:41 +0100

    * google map: set correct zoom level after fitBounds * teaser-default: respect link target (cherry picked from commit 9b64e8a)

--- views/scripts/snippet/teaser-default.php
+++ views/scripts/snippet/teaser-default.php
@@ -2,10 +2,14 @@
 $lightBoxParam = !is_null($this->getParam('useLightBox')) ? $this->getParam('useLightBox') : $this->checkbox('useLightBox')->isChecked();
 $useLightBox = $lightBoxParam && !$this->editmode;
 $hasLink = !$this->globallink('link')->isEmpty();
+$linkTarget = $hasLink ? $this->globallink('link')->getTarget() : NULL;
 ?>
 <div class="single-teaser default <?= $useLightBox ? 'light-gallery' : ''; ?>">
 
-    <?= !$this->editmode && $useLightBox ? '<a href="' . $this->image('image')->getThumbnail('lightBoxImage') . '" class="item">' : (!$this->editmode && $hasLink ? '<a href="' . $this->globallink('link')->getHref() . '" class="item">' : ''); ?>
+    <?= !$this->editmode && $useLightBox
+        ? '<a href="' . $this->image('image')->getThumbnail('lightBoxImage') . '" class="item">'
+        : (!$this->editmode && $hasLink ? '<a href="' . $this->globallink('link')->getHref() . '" ' . (!empty($linkTarget) ? 'target="' . $this->globallink('link')->getTarget(). '"' : '') . ' class="item">' : '');
+    ?>
 
     <?= $this->image('image', [
 
