e38f6b539fa54e692e0516e120b90cec4f1f23e5
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-01-06 10:38:36 +0100

    fix #11

--- views/areas/teaser/view.php
+++ views/areas/teaser/view.php
@@ -1,6 +1,6 @@
 <?php if( $this->editmode ) { ?>
     <?= \Toolbox\Tool\ElementBuilder::buildElementConfig('teaser', $this) ?>
 <?php }?>
-<div class="toolbox-teaser <?= $this->select('teaserContainerAdditionalClasses')->getData();?>">
+<div class="toolbox-teaser <?= $this->select('teaserAdditionalClasses')->getData();?>">
     <?= $this->template('toolbox/teaser.php') ?>
 </div>
