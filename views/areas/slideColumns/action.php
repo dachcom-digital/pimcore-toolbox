54960ca041ed0e2a84a21c79109050acc299e3bf
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-02-17 09:05:53 +0100

    move slideColumns config to action.php

--- views/areas/slideColumns/action.php
+++ views/areas/slideColumns/action.php
@@ -8,6 +8,23 @@ class SlideColumns extends Document\Tag\Area\AbstractArea
 {
     public function action()
     {
+        $equalHeight = $this->view->checkbox('equalHeight')->isChecked() && !$this->view->editmode;
+        $id = $this->view->brick->getId() . '-' . $this->view->brick->getIndex();
+        $slidesPerView = (int)$this->view->select('slidesPerView')->getData();
+        $slideElements = $this->view->block('slideCols', ['default' => $slidesPerView]);
+        $slidesPerViewClass = $this->view->toolboxHelper()->calculateSlideColumnClasses($slidesPerView);
+        $breakpoints = $this->view->toolboxHelper()->calculateSlideColumnBreakpoints($slidesPerView);
+
+        $this->view->assign(
+            [
+                'id'                   => $id,
+                'slideElements'        => $slideElements,
+                'slidesPerView'        => $slidesPerView,
+                'slidesPerViewClasses' => $slidesPerViewClass,
+                'breakpoints'          => $breakpoints,
+                'equalHeight'          => $equalHeight
+            ]
+        );
     }
 
     public function getBrickHtmlTagOpen($brick)
--- views/areas/slideColumns/view.php
+++ views/areas/slideColumns/view.php
@@ -1,24 +1,6 @@
 <?php if ($this->editmode) { ?>
     <?= \Toolbox\Tool\ElementBuilder::buildElementConfig('slideColumns', $this) ?>
 <?php } ?>
-<?php
-
-    $equalHeight = $this->checkbox('equalHeight')->isChecked() && !$this->editmode;
-    $id = $this->brick->getId() . '-' . $this->brick->getIndex();
-    $slidesPerView = (int)$this->select('slidesPerView')->getData();
-    $slideElements = $this->block('slideCols', ['default' => $slidesPerView]);
-    $slidesPerViewClass = $this->toolboxHelper()->calculateSlideColumnClasses($slidesPerView);
-    $breakpoints = $this->toolboxHelper()->calculateSlideColumnBreakpoints($slidesPerView);
-
-?>
-<div class="toolbox-element toolbox-slide-columns <?= $this->select('columnsAdditionalClasses')->getData(); ?><?= $equalHeight ? ' equal-height' : '' ?>">
-    <?= $this->template('toolbox/slideColumns.php',
-        [
-            'id'                   => $id,
-            'slideElements'        => $slideElements,
-            'slidesPerView'        => $slidesPerView,
-            'slidesPerViewClasses' => $slidesPerViewClass,
-            'breakpoints'          => $breakpoints
-        ]
-    ); ?>
+<div class="toolbox-element toolbox-slide-columns <?= $this->select('columnsAdditionalClasses')->getData(); ?><?= $this->equalHeight ? ' equal-height' : '' ?>">
+    <?= $this->template('toolbox/slideColumns.php'); ?>
 </div>
