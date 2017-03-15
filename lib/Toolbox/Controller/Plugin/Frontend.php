79721e73da805a5af158739bb351a2b6485c7402
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-03-15 09:45:31 +0100

    disable javascript injection in print documents. fixes #22

--- lib/Toolbox/Controller/Plugin/Frontend.php
+++ lib/Toolbox/Controller/Plugin/Frontend.php
@@ -2,10 +2,19 @@
 
 namespace Toolbox\Controller\Plugin;
 
+use \Pimcore\Model\Document;
+
 class Frontend extends \Zend_Controller_Plugin_Abstract
 {
     /**
+     * @var array
+     */
+    private $printDocTypes = ['printcontainer', 'printpage'];
+
+    /**
      * @param \Zend_Controller_Request_Abstract $request
+     *
+     * @return bool
      */
     public function preDispatch(\Zend_Controller_Request_Abstract $request)
     {
@@ -19,29 +28,40 @@ class Frontend extends \Zend_Controller_Plugin_Abstract
         $view = $renderer->view;
         $view->addScriptPath(PIMCORE_PLUGINS_PATH . '/Toolbox/views/scripts');
         $view->addHelperPath(PIMCORE_PLUGINS_PATH . '/Toolbox/lib/Toolbox/View/Helper', 'Toolbox\View\Helper');
+
+        return TRUE;
     }
 
     /**
      * @param \Zend_Controller_Request_Abstract $request
+     *
+     * @return bool
      */
     public function postDispatch(\Zend_Controller_Request_Abstract $request)
     {
         parent::postDispatch($request);
 
         $layout = \Zend_Layout::getMvcInstance();
+        $document = $request->getParam('document');
 
-        if ($layout && $layout->isEnabled() !== FALSE) {
-            \Pimcore::getEventManager()->attach('toolbox.addAsset', function (\Zend_EventManager_Event $e) {
-                $assetHandler = $e->getTarget();
-
-                $assetHandler->appendScript('toolbox-vendor-vimeo-api', '/plugins/Toolbox/static/js/frontend/vendor/vimeo-api.min.js', [], ['showInFrontEnd' => TRUE]);
-
-                $assetHandler->appendScript('toolbox-frontend-main', '/plugins/Toolbox/static/js/frontend/toolbox-main.js', [], ['showInFrontEnd' => TRUE]);
-                $assetHandler->appendScript('toolbox-frontend-video', '/plugins/Toolbox/static/js/frontend/toolbox-video.js', [], ['showInFrontEnd' => TRUE]);
-                $assetHandler->appendScript('toolbox-frontend-google-maps', '/plugins/Toolbox/static/js/frontend/toolbox-googleMaps.js', [], ['showInFrontEnd' => TRUE]);
-            });
+        if (   !$document instanceof Document
+            || !$layout instanceof \Zend_Layout
+            || !$layout->isEnabled()
+            || in_array($document->getType(), $this->printDocTypes)
+        ) {
+            return FALSE;
         }
+
+        \Pimcore::getEventManager()->attach('toolbox.addAsset', function (\Zend_EventManager_Event $e) {
+
+            $assetHandler = $e->getTarget();
+            $assetHandler->appendScript('toolbox-vendor-vimeo-api', '/plugins/Toolbox/static/js/frontend/vendor/vimeo-api.min.js', [], ['showInFrontEnd' => TRUE]);
+            $assetHandler->appendScript('toolbox-frontend-main', '/plugins/Toolbox/static/js/frontend/toolbox-main.js', [], ['showInFrontEnd' => TRUE]);
+            $assetHandler->appendScript('toolbox-frontend-video', '/plugins/Toolbox/static/js/frontend/toolbox-video.js', [], ['showInFrontEnd' => TRUE]);
+            $assetHandler->appendScript('toolbox-frontend-google-maps', '/plugins/Toolbox/static/js/frontend/toolbox-googleMaps.js', [], ['showInFrontEnd' => TRUE]);
+        });
+
+        return TRUE;
     }
 
 }
-
