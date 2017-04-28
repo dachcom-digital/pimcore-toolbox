0eeb15b424646d4ce401da4c628abc96c1aa39fa
pmoser <pmoser@dachcom.ch>
2017-04-28 10:14:14 +0200

    hotfix download previewImage

--- views/scripts/toolbox/download/list/item.php
+++ views/scripts/toolbox/download/list/item.php
@@ -2,7 +2,7 @@
 
 <li>
     <a href="<?= $fileInfo['path']; ?>" <?= $this->toolboxHelper()->addTracker('download', $this->download); ?> target="_blank" class="icon-download-<?= $fileInfo['type']; ?>">
-        <?php if ($this->showPreviewImages) { ?><span class="preview-image"><img src="<?= ['previewImage'] ?>" alt="<?= $fileInfo['altText'] ?>"/></span><?php } ?>
+        <?php if ($this->showPreviewImages) { ?><span class="preview-image"><img src="<?= $fileInfo['previewImage'] ?>" alt="<?= $fileInfo['altText'] ?>"/></span><?php } ?>
         <span class="title"><?= $fileInfo['name']; ?></span>
         <?php if ($this->showFileInfo) { ?> <span class="file-info">(<span class="file-type"><?= $fileInfo['type'] ?></span>, <?= $fileInfo['size'] ?>)</span><?php } ?>
     </a>
