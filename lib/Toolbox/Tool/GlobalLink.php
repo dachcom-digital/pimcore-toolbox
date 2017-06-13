0b6432e8bfb66e73dbf5fce07b472c3a17b825f4
Stefan Hagspiel <shagspiel@dachcom.ch>
2017-06-13 19:15:23 +0200

    improve GlobalLink parser

--- lib/Toolbox/Tool/GlobalLink.php
+++ lib/Toolbox/Tool/GlobalLink.php
@@ -3,10 +3,10 @@
 namespace Toolbox\Tool;
 
 use Pimcore\Tool;
-use Toolbox\Config;
 
 class GlobalLink
 {
+
     /**
      * @param      $path
      * @param bool $checkRequestUri
@@ -21,62 +21,74 @@ class GlobalLink
         if (\Zend_Registry::isRegistered('Website_Country')) {
             $currentCountry = \Zend_Registry::get('Website_Country');
         } else if ($checkRequestUri) {
-            $currentCountry = self::checkRequestUri();
+            $currentCountry = self::checkRequestUri('country');
+        }
+
+        $currentLanguage = NULL;
+
+        if (\Zend_Registry::isRegistered('Zend_Locale')) {
+            $currentLanguage = (string)\Zend_Registry::get('Zend_Locale');
+        } else if ($checkRequestUri) {
+            $currentLanguage = self::checkRequestUri('language');
         }
 
         //only parse if country in l10n is active!
-        if (!is_null($currentCountry)) {
-            $validLanguages = Tool::getValidLanguages();
-
-            $currentIsoCode = strtolower($currentCountry);
-            $shiftCountry = FALSE;
-
-            $pathCountry = '';
-            $globalString = 'global';
-
-            $urlPath = parse_url($path, PHP_URL_PATH);
-            $urlPathFragments = explode('/', ltrim($urlPath, '/'));
-
-            //it's a global page, link is correct.
-            if ($currentIsoCode === $globalString) {
-                return $path;
-            }
-
-            if (isset($urlPathFragments[0])) {
-                $pathElements = explode('-', $urlPathFragments[0]);
-
-                //first needs to be country
-                $pathCountry = isset($pathElements[0]) ? $pathElements[0] : NULL;
-
-                //second needs to be language.
-                $pathLanguage = isset($pathElements[1]) ? $pathElements[1] : NULL;
-
-                $isValidLanguage = in_array($pathLanguage, $validLanguages);
-
-                //if 2. fragment is invalid language and 1. fragment is valid language, 1. fragment is missing!
-                $shiftCountry = $isValidLanguage == FALSE && in_array($pathCountry, $validLanguages);
-            }
-
-            //country is missing. add it.
-            if ($shiftCountry) {
-                $path = '/' . $currentIsoCode . '-' . ltrim($path, '/');
-            } //it's a global page with "global-" in string. change it.
-            else if (substr($path, 0, strlen($globalString) + 2) === '/' . $globalString . '-') {
-                $path = substr_replace($path, '/' . $currentIsoCode . '-', 0, strlen($globalString) + 2);
-            }
-
+        if (is_null($currentCountry)) {
             return $path;
+        }
+
+        $validLanguages = Tool::getValidLanguages();
+
+        $currentIsoCode = strtolower($currentCountry);
+        $currentLangCode = strtolower($currentLanguage);
+
+        $globalString = 'global';
+
+        $urlPath = parse_url($path, PHP_URL_PATH);
+        $urlPathFragments = explode('/', ltrim($urlPath, '/'));
+
+        //it's a global page, link is correct.
+        if ($currentIsoCode === $globalString) {
+            return $path;
+        }
+
+        if (empty($urlPathFragments)) {
+            return $path;
+        }
+
+        //explode first path fragment, assuming that the first part is language/country slug
+        $pathElements = explode('-', $urlPathFragments[0]);
+
+        //first needs to be country
+        $pathCountry = isset($pathElements[0]) ? strtolower($pathElements[0]) : NULL;
+
+        //second needs to be language.
+        $pathLanguage = isset($pathElements[1]) ? strtolower($pathElements[1]) : NULL;
+
+        //if 1. fragment is valid language and 2. fragment is invalid language, 1. fragment is missing!
+        if (!in_array($pathLanguage, $validLanguages) && in_array($pathCountry, $validLanguages)) {
+            return '/' . $currentIsoCode . '-' . ltrim($path, '/');
+        }
+
+        //it's a global page with "global-" in string. change it.
+        if (substr($path, 0, strlen($globalString) + 2) === '/' . $globalString . '-') {
+            return substr_replace($path, '/' . $currentIsoCode . '-', 0, strlen($globalString) + 2);
+        }
+
+        //wrong country, right language
+        if (!is_null($pathCountry) && $pathCountry !== $currentIsoCode && strtolower($pathLanguage) === $currentLangCode) {
+            $path = substr_replace($path, '/' . $currentIsoCode . '-', 0, strlen('/' . $pathCountry . '-'));
+        } //right language, wrong country
+        else if (!is_null($pathLanguage) && $pathLanguage !== $currentLangCode && (!empty($pathCountry) && $pathCountry === $currentIsoCode)) {
+            $path = substr_replace($path, '/' . $currentIsoCode . '-' . $currentLangCode, 0, strlen('/' . $pathCountry . '-' . $pathLanguage));
         }
 
         return $path;
     }
 
-    /**
-     * @return string
-     */
-    private static function checkRequestUri()
+    private static function checkRequestUri($fragment = 'country')
     {
-        $currentCountry = 'GLOBAL';
+        $currentFragment = $fragment === 'country' ? 'GLOBAL' : '';
 
         if (isset($_SERVER['REQUEST_URI']) && !empty($_SERVER['REQUEST_URI'])) {
             $urlPath = parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
@@ -86,12 +98,12 @@ class GlobalLink
                 $slug = explode('-', $urlPathFragments[0]);
 
                 if (count($slug) === 2) {
-                    $currentCountry = $slug[0];
+                    $currentFragment = $fragment === 'country' ? $slug[0] : $slug[1];
                 }
             }
         }
 
-        return $currentCountry;
+        return $currentFragment;
     }
 
 }
