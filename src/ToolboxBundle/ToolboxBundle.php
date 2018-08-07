7d7649df1761912807a7cfd37113d8645d98b7a5
Stefan Hagspiel <shagspiel@dachcom.ch>
2018-08-07 14:52:19 +0200

    bump version

--- src/ToolboxBundle/ToolboxBundle.php
+++ src/ToolboxBundle/ToolboxBundle.php
@@ -10,7 +10,7 @@ use Symfony\Component\DependencyInjection\ContainerBuilder;
 
 class ToolboxBundle extends AbstractPimcoreBundle
 {
-    const BUNDLE_VERSION = '2.6.0';
+    const BUNDLE_VERSION = '2.6.1';
 
     /**
      * @inheritDoc
