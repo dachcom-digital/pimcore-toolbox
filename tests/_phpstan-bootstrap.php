2389d42fb33e503fe2e53a2b52983b19a00b223a
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-02-10 16:30:27 +0100

    add class alias to phpstan bootstrap

--- tests/_phpstan-bootstrap.php
+++ tests/_phpstan-bootstrap.php
@@ -4,3 +4,7 @@
 if (!defined('PIMCORE_PRIVATE_VAR')) {
     define('PIMCORE_PRIVATE_VAR', '');
 }
+
+/** hide class alias in IDE code completion */
+$multiHrefClass = '\\Pimcore\\Model\\Document\\Tag\\Multihref';
+class_alias(\Pimcore\Model\Document\Tag\Relations::class, $multiHrefClass, true);
