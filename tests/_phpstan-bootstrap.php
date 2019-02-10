ea0156bbd73333db72423438713928a957fa20fa
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-02-10 16:06:16 +0100

    ignore constant calls from dependency bundle

--- tests/_phpstan-bootstrap.php
+++ tests/_phpstan-bootstrap.php
@@ -4,12 +4,3 @@
 if (!defined('PIMCORE_PRIVATE_VAR')) {
     define('PIMCORE_PRIVATE_VAR', '');
 }
-
-if (!defined('PROTECTED_ASSET_FOLDER')) {
-    define('PROTECTED_ASSET_FOLDER', '');
-}
-
-if (!defined('RESTRICTION_SECTION_ALLOWED')) {
-    define('RESTRICTION_SECTION_ALLOWED', '');
-}
-
