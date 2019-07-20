0a91cadf2f5d159e9c75356d4c2c40c996116836
Stefan Hagspiel <shagspiel@dachcom.ch>
2019-07-20 11:39:48 +0200

    update test structure

--- tests/_support/App/TestAppKernel.php
+++ tests/_support/App/TestAppKernel.php
@@ -2,29 +2,17 @@
 
 namespace DachcomBundle\Test\App;
 
+use Pimcore\Kernel;
 use DachcomBundle\Test\DependencyInjection\MakeServicesPublicPass;
 use DachcomBundle\Test\DependencyInjection\MonologChannelLoggerPass;
 use Pimcore\HttpKernel\BundleCollection\BundleCollection;
-use Pimcore\Kernel;
-use Symfony\Component\Config\Loader\LoaderInterface;
+use Symfony\Component\Config\FileLocator;
 use Symfony\Component\DependencyInjection\Compiler\PassConfig;
 use Symfony\Component\DependencyInjection\ContainerBuilder;
+use Symfony\Component\DependencyInjection\Loader\YamlFileLoader;
 
 class TestAppKernel extends Kernel
 {
-    /**
-     * {@inheritdoc}
-     */
-    public function registerContainerConfiguration(LoaderInterface $loader)
-    {
-        parent::registerContainerConfiguration($loader);
-
-        $runtimeConfigDir = codecept_data_dir() . 'config' . DIRECTORY_SEPARATOR;
-        $runtimeConfigDirConfig = $runtimeConfigDir . DIRECTORY_SEPARATOR . 'config.yml';
-
-        $loader->load($runtimeConfigDirConfig);
-    }
-
     /**
      * {@inheritdoc}
      */
@@ -40,6 +28,8 @@ class TestAppKernel extends Kernel
 
     /**
      * @param ContainerBuilder $container
+     *
+     * @throws \Exception
      */
     protected function build(ContainerBuilder $container)
     {
@@ -47,6 +37,10 @@ class TestAppKernel extends Kernel
             PassConfig::TYPE_BEFORE_OPTIMIZATION, -100000);
         $container->addCompilerPass(new MonologChannelLoggerPass(),
             PassConfig::TYPE_BEFORE_OPTIMIZATION, 1);
+
+        $runtimeConfigDir = codecept_data_dir() . 'config' . DIRECTORY_SEPARATOR;
+        $loader = new YamlFileLoader($container, new FileLocator([$runtimeConfigDir]));
+        $loader->load('config.yml');
     }
 
     /**
